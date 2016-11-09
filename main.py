7e0d08acc79e0785c99f7b4b580aee6929661f7a
btan <atathunder@gmail.com>
2016-11-09 16:20:43 +0800

    add-visual-mul-ver

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import numpy as np
 from dealCsv import readCsv,writeCsv
 from gradientDescent import gradientDescent
-from plot import plotLine,plotLoss
+from plot import plotLine,plotLoss,plot3D
 from featureNormalize import featureNormalize
 
 loadfile = "ex1data2.csv"
-alpha = 0.01
-iterations = 500
+alpha = 0.001
+iterations = 1500
 
 def simpleLinerRegression(data):
     X = np.c_[data[:, :-1], np.ones(shape=data.shape[0])]
@@ -21,14 +21,21 @@ def simpleLinerRegression(data):
 
 def mulLinerRegression(data):
     X = data[:,:-1]
-    X = np.c_[featureNormalize(X), np.ones(shape=data.shape[0])]
+    X_norm, mu, sigma = featureNormalize(X)
+    X_norm = np.c_[X_norm, np.ones(shape=data.shape[0])]
     y = data[:, -1]
     theta = np.zeros(shape=data.shape[1])
 
-    theta, loss = gradientDescent(X, y, theta, alpha=alpha, iterations=iterations)
+    theta, loss = gradientDescent(X_norm, y, theta, alpha=alpha, iterations=iterations)
     plotLoss(loss, iterations)
-    print theta
+    print theta, mu, sigma
 
+    plot3D(data, theta, mu, sigma)
+
+    # test
+    x = [[1380,3],[1494,3],[1940,4]]
+    x = np.c_[(x - mu) / sigma, np.ones(3)]
+    print np.dot(x, theta)
 
 if __name__ == '__main__':
     data = readCsv(loadfile)
--- plot.py
+++ plot.py
@@ -1,5 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import itertools
+from mpl_toolkits.mplot3d import Axes3D
+from featureNormalize import featureNormalize
 
 def plotLine (data ,theta):
     a = np.floor(np.min(data, axis=0))
@@ -12,6 +15,24 @@ def plotLine (data ,theta):
     plt.plot(x,Y)
     plt.show()
 
+def plot3D (data, theta, mu, sigma):
+    x, y, z = data[:,0], data[:,1], data[:,2]
+    a = np.floor(np.min(data, axis=0))
+    b = np.floor(np.max(data, axis=0)) + 1
+    x_s = np.linspace(a[0], b[0], 100)
+    y_s = np.linspace(a[1], b[1], 100)
+    X, Y = np.meshgrid(x_s, y_s)
+    X_s = list(itertools.chain.from_iterable(X))
+    Y_s = list(itertools.chain.from_iterable(Y))
+
+    Z = np.dot(np.c_[(np.c_[X_s, Y_s]-mu)/sigma, np.ones(10000)], theta)
+
+    ax = plt.subplot(111, projection='3d')
+    ax.plot_surface(X, Y, Z.reshape(100,100), rstride=1, cstride=1, cmap='rainbow')
+    ax.scatter(x,y,z)
+    ax.set_zlabel('Z')
+    plt.show()
+
 def plotLoss (loss, iterations):
     plt.plot(range(iterations), loss)
-    plt.show()
+    # plt.show()
