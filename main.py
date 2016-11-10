f819a996bb3995c3cf225d4f88fa41d670682850
btan <atathunder@gmail.com>
2016-11-10 17:19:28 +0800

    add-visual

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import numpy as np
 from dealCsv import readCsv,writeCsv
 from gradientDescent import gradientDescent
-from plot import plotLine,plotLoss,plot3D,plotSortScatter,plotSortLine,plotShow
+from plot import plotLine,plotLoss,plot3D,plotSortScatter,plotSortLine,plotShow,plotSortBlock
 from featureNormalize import featureNormalize
 from activations import sigmoid
 from featuremap import mapFeature
 
 """
-for ex2data2: alpha = 0.0001 and degree = 6
+for ex2data2: alpha = 0.0001 and degree = 6 and initial theta all zero.
               predict reach 0.6525423728881 when iterations reach 200000 then not change.
+              we can initial theta all one to reach higher accurate, but they all came to the same end
 """
 
 loadfile = "data/ex2data2.csv"
@@ -72,21 +73,27 @@ def logicRegressionRegularized(data):
     y = data[:,-1]
     theta = np.zeros(shape=X.shape[1])
 
-    # theta, loss = gradientDescent(X, y, theta, options)
-    for i in range(100):
+    theta, loss = gradientDescent(X, y, theta, options)
+    accurates = []
+    for i in range(25):
         theta, _ = gradientDescent(X, y, theta, options)
         # test
         predict = (np.round(sigmoid(np.dot(X, theta))) == y)
-        print i * options["iterations"], 1.0 * np.sum(predict == True) / len(y)
+        accurate = 1.0 * np.sum(predict == True) / len(y)
+        accurates.append(accurate)
+        print i * options["iterations"], accurate
     # print theta
 
     # test
-    predict = (np.round(sigmoid(np.dot(X, theta))) == y)
-    print 1.0 * np.sum(predict == True) / len(y)
-    print 1.0 * np.sum(y==0) / len(y)
+    # predict = (np.round(sigmoid(np.dot(X, theta))) == y)
+    # print 1.0 * np.sum(predict == True) / len(y)
+    # print 1.0 * np.sum(y==0) / len(y)
 
+    # plotLoss(accurates, 50)
     # plotLoss(loss, options["iterations"])
-    # plotShow()
+    plotSortBlock(data,theta)
+    plotSortScatter(data)
+    plotShow()
 
 if __name__ == '__main__':
     data = readCsv(loadfile)
--- plot.py
+++ plot.py
@@ -1,6 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import itertools
+from featuremap import mapFeature
+from activations import sigmoid
 
 def plotLine (data ,theta):
     a = np.floor(np.min(data, axis=0))
@@ -34,7 +36,7 @@ def plot3D (data, theta, mu, sigma):
 def plotSortScatter (data):
     zeroset = [i.tolist()[:-1] for i in data if i[-1]==0]
     oneset = [i.tolist()[:-1] for i in data if i[-1]==1]
-    plt.plot([i[0] for i in zeroset], [i[1] for i in zeroset], 'gs',
+    plt.plot([i[0] for i in zeroset], [i[1] for i in zeroset], 'rs',
                 [i[0] for i in oneset], [i[1] for i in oneset], 'b^')
 
 def plotSortLine(data, theta):
@@ -42,6 +44,21 @@ def plotSortLine(data, theta):
     x = np.array([np.floor(np.min(data[:,0])), np.floor(np.max(data[:,0])) + 1])
     Y = - (theta[0] * x + theta[2]) / theta[1]
     plt.plot(x, Y)
+
+def plotSortBlock(data, theta):
+    a = np.floor(np.min(data, axis=0))
+    b = np.floor(np.max(data, axis=0)) + 1
+    x_s = np.linspace(a[0], b[0], 100)
+    y_s = np.linspace(a[1], b[1], 100)
+    X, Y = np.meshgrid(x_s, y_s)
+    X_s = list(itertools.chain.from_iterable(X))
+    Y_s = list(itertools.chain.from_iterable(Y))
+
+    Z =  np.c_[X_s, Y_s, sigmoid(np.dot(mapFeature(np.c_[X_s, Y_s], 6),theta))]
+    Z = [i for i in Z if np.round(i[2])==0]
+    Z = np.array(Z)
+    print type(Z)
+    plt.plot(Z[:,0],Z[:,1],'go')
 
 def plotShow():
     plt.show()
